2ea02dbecdaa68bc25211edecf866e2585525273
riet <loli_520@126.com>
2024-07-02 12:05:24 +0800

    ConcurrentPool

--- pool.go
+++ pool.go
@@ -3,6 +3,8 @@ package gpool
 import (
 	"context"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -45,16 +47,20 @@ func (l *Limiter) Wait() {
 	_ = l.limiter.Wait(context.Background())
 }
 
+func (l *Limiter) WaitN(n int) {
+	_ = l.limiter.WaitN(context.Background(), min(n, l.Burst()))
+}
+
 // NewLimiter limit is normal QPS and allows bursts of up to burst to exceed the normal QPS,
 func NewLimiter(limit, burst int) *Limiter {
 	return &Limiter{limiter: rate.NewLimiter(rate.Limit(min(limit, burst)), burst)}
 }
 
-type Pool struct {
+type LimiterPool struct {
 	*Limiter
 }
 
-func (p *Pool) run(wg *sync.WaitGroup, f func(any), v any, onPanic func(any, any)) {
+func (l *LimiterPool) run(wg *sync.WaitGroup, f func(any), v any, onPanic func(any, any)) {
 	defer wg.Done()
 	defer func() {
 		if err := recover(); err != nil && onPanic != nil {
@@ -64,45 +70,119 @@ func (p *Pool) run(wg *sync.WaitGroup, f func(any), v any, onPanic func(any, any
 	f(v)
 }
 
-func (p *Pool) Run(f func(any), v []any, onPanic func(any, any)) {
+func (l *LimiterPool) Run(f func(any), v []any, onPanic func(any, any)) {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(v))
 	for _, i := range v {
-		p.Wait()
-		go p.run(wg, f, i, onPanic)
+		l.Wait()
+		go l.run(wg, f, i, onPanic)
 	}
 	wg.Wait()
 }
 
-func NewPool(l *Limiter) *Pool {
-	return &Pool{l}
+func NewLimiterPool(l *Limiter) *LimiterPool {
+	return &LimiterPool{l}
 }
 
-type GenericPool[T any] struct {
+type GenericLimiterPool[T any] struct {
 	*Limiter
 	f func(T)
 }
 
-func (p *GenericPool[T]) run(wg *sync.WaitGroup, v T, onPanic func(T, any)) {
+func (g *GenericLimiterPool[T]) run(wg *sync.WaitGroup, v T, onPanic func(T, any)) {
 	defer wg.Done()
 	defer func() {
 		if err := recover(); err != nil && onPanic != nil {
 			onPanic(v, err)
 		}
 	}()
-	p.f(v)
+	g.f(v)
 }
 
-func (p *GenericPool[T]) Run(v []T, onPanic func(T, any)) {
+func (g *GenericLimiterPool[T]) Run(v []T, onPanic func(T, any)) {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(v))
 	for _, i := range v {
-		p.Wait()
-		go p.run(wg, i, onPanic)
+		g.Wait()
+		go g.run(wg, i, onPanic)
 	}
 	wg.Wait()
 }
 
-func NewGenericPool[T any](l *Limiter, f func(T)) *GenericPool[T] {
-	return &GenericPool[T]{l, f}
+func NewGenericLimiterPool[T any](l *Limiter, f func(T)) *GenericLimiterPool[T] {
+	return &GenericLimiterPool[T]{l, f}
+}
+
+// ConcurrentPool is different from LimiterPool it can limit the maximum concurrency when running
+type ConcurrentPool struct {
+	*Limiter
+}
+
+func (c *ConcurrentPool) run(max *int64, wg *sync.WaitGroup, f func(any), v any, onPanic func(any, any)) {
+	defer wg.Done()
+	defer atomic.AddInt64(max, 1)
+	defer func() {
+		if err := recover(); err != nil && onPanic != nil {
+			onPanic(v, err)
+		}
+	}()
+	f(v)
+}
+
+func (c *ConcurrentPool) Run(max int64, f func(any), v []any, onPanic func(any, any)) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(v))
+	for _, i := range v {
+		c.Wait()
+		for {
+			if atomic.LoadInt64(&max) > 0 {
+				atomic.AddInt64(&max, -1)
+				go c.run(&max, wg, f, i, onPanic)
+				break
+			}
+			time.Sleep(time.Second)
+		}
+	}
+	wg.Wait()
+}
+
+func NewConcurrentPool(l *Limiter) *ConcurrentPool {
+	return &ConcurrentPool{l}
+}
+
+type GenericConcurrentPool[T any] struct {
+	*Limiter
+	f func(T)
+}
+
+func (g *GenericConcurrentPool[T]) run(max *int64, wg *sync.WaitGroup, v T, onPanic func(T, any)) {
+	defer wg.Done()
+	defer atomic.AddInt64(max, 1)
+	defer func() {
+		if err := recover(); err != nil && onPanic != nil {
+			onPanic(v, err)
+		}
+	}()
+	g.f(v)
+}
+
+func (g *GenericConcurrentPool[T]) Run(max int64, v []T, onPanic func(T, any)) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(v))
+	for _, i := range v {
+		g.Wait()
+		for {
+			if atomic.LoadInt64(&max) > 0 {
+				atomic.AddInt64(&max, -1)
+				go g.run(&max, wg, i, onPanic)
+				break
+			}
+			time.Sleep(time.Second)
+		}
+	}
+	wg.Wait()
+}
+
+func NewGenericConcurrentPool[T any](l *Limiter, f func(T)) *GenericConcurrentPool[T] {
+	return &GenericConcurrentPool[T]{l, f}
 }
--- pool_test.go
+++ pool_test.go
@@ -7,10 +7,10 @@ import (
 	"time"
 )
 
-var limiter = NewLimiter(2, 10)
+var limiter = NewLimiter(10, 10)
 
-func TestPool(t *testing.T) {
-	p := NewPool(limiter)
+func TestLimiterPool(t *testing.T) {
+	p := NewLimiterPool(limiter)
 	var items []any
 	for i := 1; i < 101; i++ {
 		items = append(items, i)
@@ -21,13 +21,14 @@ func TestPool(t *testing.T) {
 	}
 	f := func(i any) {
 		log.Println(i)
+		time.Sleep(5 * time.Second)
 	}
 	go p.Run(f, items, nil)
 	p.Run(f, items2, nil)
 }
 
-func TestGenericPool(t *testing.T) {
-	p := NewGenericPool[int](limiter, func(i int) {
+func TestGenericLimiterPool(t *testing.T) {
+	p := NewGenericLimiterPool[int](limiter, func(i int) {
 		log.Println(i)
 		time.Sleep(time.Second)
 	})
@@ -42,4 +43,39 @@ func TestGenericPool(t *testing.T) {
 		fmt.Println(p.Limit(), p.Burst())
 	}()
 	p.Run(items, nil)
+}
+
+func TestConcurrentPool(t *testing.T) {
+	p := NewConcurrentPool(limiter)
+	var items []any
+	for i := 1; i < 101; i++ {
+		items = append(items, i)
+	}
+	var items2 []any
+	for i := 101; i < 201; i++ {
+		items2 = append(items2, i)
+	}
+	f := func(i any) {
+		log.Println(i)
+		time.Sleep(5 * time.Second)
+	}
+	go p.Run(8, f, items, nil)
+	p.Run(2, f, items2, nil)
+}
+
+func TestGenericConcurrentPool(t *testing.T) {
+	p := NewGenericConcurrentPool(limiter, func(i int) {
+		log.Println(i)
+		time.Sleep(3 * time.Second)
+	})
+	var items []int
+	for i := 1; i < 101; i++ {
+		items = append(items, i)
+	}
+	var items2 []int
+	for i := 101; i < 201; i++ {
+		items2 = append(items2, i)
+	}
+	go p.Run(6, items, nil)
+	p.Run(4, items2, nil)
 }
